d1dce883550e8b4f352c74ef8467eee2c8a73246
arya-sianati <aryasianati@gmail.com>
2023-05-12 18:01:25 -0700

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Arya Sianati on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
