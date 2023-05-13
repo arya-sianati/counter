592de14d76ed8917be427aafabd6c023b1979031
arya-sianati <aryasianati@gmail.com>
2023-05-12 18:16:42 -0700

    Initial Commit

--- counter/ContentView.swift
+++ counter/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var i = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            
+
+            Text(String(i)).font(.largeTitle)
+                .foregroundColor(Color.blue).padding(.bottom, 50.0)
+                
+            
+            
+            HStack
+            {
+                Button {
+                    neg()
+                } label: {
+                    Image(systemName:"minus.circle").font(.title)
+                }.padding()
+                
+                Button {
+                    add()
+                } label: {
+                    Image(systemName:"plus.circle").font(.title)
+                }.padding()
+                
+            }.padding(.top, 50.0)
+            
+
+            
         }
-        .padding()
+        
+    }
+    
+    func add()
+    {
+        i += 1
+    }
+    func neg()
+    {
+        if i>0
+        {
+            i -= 1
+        }
     }
 }
 
